3ee52b67c2b3d1a4fc8b8f223adfa9d91e20712e
Jim Rhines <jimrhines@gmail.com>
2020-04-20 21:40:27 -0500

    Layout updates to default template

--- wp-content/themes/understrap-child/loop-templates/content-page.php
+++ wp-content/themes/understrap-child/loop-templates/content-page.php
@@ -9,37 +9,14 @@
 defined( 'ABSPATH' ) || exit;
 ?>
 
-<article <?php post_class(); ?> id="post-<?php the_ID(); ?>">
+<?php if( get_field('featured_image_display') == 'No' ) {
+	echo get_the_post_thumbnail( $post->ID, 'large' );
+} ?>
 
-	<header class="entry-header">
-
+<div class="row">
+	<div class="col-md-8">
 		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
-
-	</header><!-- .entry-header -->
-	
-	<?php if( get_field('featured_image_display') == 'No' ) {
-		echo get_the_post_thumbnail( $post->ID, 'large' );
-	} ?>
-
-	<div class="entry-content">
-
 		<?php the_content(); ?>
-
-		<?php
-		wp_link_pages(
-			array(
-				'before' => '<div class="page-links">' . __( 'Pages:', 'understrap' ),
-				'after'  => '</div>',
-			)
-		);
-		?>
-
-	</div><!-- .entry-content -->
-
-	<footer class="entry-footer">
-
-		<?php edit_post_link( __( 'Edit', 'understrap' ), '<span class="edit-link">', '</span>' ); ?>
-
-	</footer><!-- .entry-footer -->
-
-</article><!-- #post-## -->
+	</div>
+	<div class="col-md-4"></div>
+</div>
